0991939bf8a1cec977b6b540412e83df61d22283
KIARA <kiarazhu386@gmail.com>
2025-05-05 06:39:35 +1000

    Update single_digit.c

--- single_digit.c
+++ single_digit.c
@@ -6,20 +6,20 @@ uint8_t seven_seg[10] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 111};
 int main(void) {
 	
 	// Set port C pins to be inputs
-	DDRC = 0;    // Already 0 by default; can omit
+	DDRC = 0x00;    // Default value; can omit
 	
 	// Set port A pins to be outputs
 	DDRA = 0xFF;
 	
-	uint8_t digit;
+	uint8_t input;
 	
 	while(1) {
 		/* Reads pins C0-C3 (via bitmasking) to
-		obtain the digit to display. */
-		digit = PINC & 0x0F;
+		obtain the 4-bit input. */
+		input = PINC & 0x0F;
 		
-		/* If digit is within 0-9 (inclusive),
-		display the digit. 
+		/* If input is a single digit (0-9 inclusive),
+		display the digit.
 		Otherwise, display nothing. */
 		if (digit < 10) {
 			PORTA = seven_seg[digit];
