8b32c2e80779ccfd2bf4e36374c25559cb4e7362
KIARA <kiarazhu386@gmail.com>
2025-05-05 07:15:20 +1000

    Update single_digit.c comments

--- single_digit.c
+++ single_digit.c
@@ -1,30 +1,24 @@
 #include <avr/io.h>
 
-// Seven segment display values for digits 0 - 9
+// Seven segment display values for digits 0 to 9
 uint8_t seven_seg[10] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 111};
 
 int main(void) {
-	
 	// Set port C pins to be inputs
 	DDRC = 0x00;    // Default value; can omit
 	
 	// Set port A pins to be outputs
 	DDRA = 0xFF;
-	
+
 	uint8_t input;
 	
 	while(1) {
-		/* Reads pins C0-C3 (via bitmasking) to
-		obtain the 4-bit input. */
-		input = PINC & 0x0F;
+		input = PINC & 0x0F;    // Read pins C0-C3 via bitmasking
 		
-		/* If input is a single digit (0-9 inclusive),
-		display the digit.
-		Otherwise, display nothing. */
 		if (digit < 10) {
-			PORTA = seven_seg[digit];
+			PORTA = seven_seg[digit];    // Display the digit
 			} else {
-			PORTA = 0;
+			PORTA = 0;    // Display nothing
 		}
 	}
 }
